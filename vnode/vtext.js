4c633b2428c70564e32a731eeb2ca46a3f3db234
amnhh <2570079254@qq.com>
2019-03-16 15:04:57 +0800

    添加 VTtxtC 注释

--- vnode/vtext.js
+++ vnode/vtext.js
@@ -2,9 +2,16 @@ var version = require("./version")
 
 module.exports = VirtualText
 
+/**
+ * VText 构造函数
+ * @param text
+ * @constructor
+ */
 function VirtualText(text) {
+    // 只是简单地把 text 变量保存到实例的 text 属性中
     this.text = String(text)
 }
 
+// 并给每一个 VText 实例都给予 version 属性和 type 属性
 VirtualText.prototype.version = version
 VirtualText.prototype.type = "VirtualText"
